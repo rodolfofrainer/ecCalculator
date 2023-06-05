3b23b346104bbd47dfda0f11c61fd16ec5dbc8f6
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-06-05 10:02:22 +0100

    add modal, fix bug regarding calculation

--- script.js
+++ script.js
@@ -1,79 +1,105 @@
-var workers = [];
+// Get the necessary elements
+const calculateBtn = document.getElementById("calculate_btn");
+const kitchenShareEl = document.getElementById("kitchen_share");
+const personSharesEl = document.getElementById("person_shares");
+const personValuePerHourEl = document.getElementById("person_value_per_hour");
+const addPersonBtn = document.getElementById("add_person_btn");
 
-function addPerson() {
-  var name = document.getElementById("person_name").value;
-  var hoursWorked = parseFloat(document.getElementById("hours_worked").value);
-  
-  workers.push({ name: name, hoursWorked: hoursWorked });
-  
-  document.getElementById("person_name").value = '';
-  document.getElementById("hours_worked").value = '';
-  
-  displayPersons();
+// Event listener for the calculate button
+calculateBtn.addEventListener("click", () => {
+  // Get the total tips and kitchen percentage values
+  const totalTips = parseFloat(document.getElementById("total_tips").value);
+  const kitchenPercentage = parseFloat(
+    document.querySelector(".active.kitchen-percentage-btn").textContent
+  );
+
+  // Calculate the kitchen share and person shares
+  const kitchenShare = (totalTips * (kitchenPercentage / 100)).toFixed(2);
+  const personShares = calculatePersonShares(totalTips, kitchenPercentage);
+
+  // Calculate the average value per hour for all employees
+  const valuePerHour = calculateAverageValuePerHour(personShares);
+
+  // Update the results display
+  kitchenShareEl.textContent = `Kitchen Share: $${kitchenShare}`;
+  personSharesEl.innerHTML = personShares.map((share) => `<li>${share}</li>`).join("");
+  personValuePerHourEl.textContent = `Average Value per Hour: $${valuePerHour}`;
+
+  // Show the modal
+  $('#resultsModal').modal('show');
+});
+
+// Function to calculate the person shares
+function calculatePersonShares(totalTips, kitchenPercentage) {
+  const personListItems = document.querySelectorAll("#person_list li");
+  const personShares = [];
+
+  // Calculate the total hours worked
+  let totalHoursWorked = 0;
+  personListItems.forEach((item) => {
+    const hoursWorked = parseFloat(item.querySelector(".hours-worked").textContent);
+    totalHoursWorked += hoursWorked;
+  });
+
+  personListItems.forEach((item) => {
+    const personName = item.querySelector(".person-name").textContent;
+    const hoursWorked = parseFloat(item.querySelector(".hours-worked").textContent);
+    const personShare = (
+      ((totalTips - totalTips * (kitchenPercentage / 100)) / totalHoursWorked) *
+      hoursWorked
+    ).toFixed(2);
+    personShares.push(`${personName}: $${personShare}`);
+  });
+
+  return personShares;
 }
 
-function removePerson(index) {
-  workers.splice(index, 1);
-  displayPersons();
+// Function to calculate the average value per hour for all employees
+function calculateAverageValuePerHour(personShares) {
+  const personListItems = document.querySelectorAll("#person_list li");
+  let totalPersonShares = 0;
+  let totalHoursWorked = 0;
+
+  personListItems.forEach((item, index) => {
+    const hoursWorked = parseFloat(item.querySelector(".hours-worked").textContent);
+    const personShare = parseFloat(personShares[index].split(": $")[1]);
+    totalPersonShares += personShare;
+    totalHoursWorked += hoursWorked;
+  });
+
+  const averageValuePerHour = (totalPersonShares / totalHoursWorked).toFixed(2);
+  return averageValuePerHour;
 }
 
-function displayPersons() {
-  var list = document.getElementById("person_list");
-  list.innerHTML = '';
+// Event listener for the add person button
+addPersonBtn.addEventListener("click", () => {
+  const personNameInput = document.querySelector(".person-name");
+  const hoursWorkedInput = document.querySelector(".hours-worked");
+  const personName = personNameInput.value.trim();
+  const hoursWorked = hoursWorkedInput.value.trim();
 
-  workers.forEach(function (worker, index) {
-    var listItem = document.createElement("li");
-    var workerName = capitalizeName(worker.name);
-    listItem.textContent = workerName + " - " + worker.hoursWorked + " hours worked";
+  if (personName !== "" && hoursWorked !== "") {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+      <div class="person-item">
+        <span class="person-name">${personName}</span> - 
+        <span class="hours-worked">${hoursWorked}</span> hours
+        <button class="remove-person-btn btn btn-danger">Remove</button>
+      </div>
+    `;
+    document.getElementById("person_list").appendChild(listItem);
 
-    var removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener("click", function () {
-      removePerson(index);
+    personNameInput.value = "";
+    hoursWorkedInput.value = "";
+  }
+});
+
+// Kitchen percentage button click event
+document.querySelectorAll(".kitchen-percentage-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelectorAll(".kitchen-percentage-btn").forEach((btn) => {
+      btn.classList.remove("active");
     });
-
-    listItem.appendChild(removeButton);
-    list.appendChild(listItem);
+    btn.classList.add("active");
   });
-}
-
-function capitalizeName(name) {
-  var names = name.split(" ");
-  var capitalizedNames = names.map(function (namePart) {
-    return namePart.charAt(0).toUpperCase() + namePart.slice(1);
-  });
-  return capitalizedNames.join(" ");
-}
-
-function calculateTips() {
-  var totalTips = parseFloat(document.getElementById("total_tips").value);
-  var kitchenPercentage = parseFloat(document.getElementById("kitchen_percentage").value);
-  
-  var kitchenTips = totalTips * (kitchenPercentage / 100);
-  var totalHoursWorked = workers.reduce((sum, worker) => sum + worker.hoursWorked, 0);
-  
-  var serverTips = totalTips - kitchenTips;
-  var workerTips = serverTips / totalHoursWorked;
-  
-  var kitchenInfo = document.createElement("p");
-  kitchenInfo.textContent = "Kitchen Tips: $" + kitchenTips.toFixed(2);
-  
-  var serverInfo = document.createElement("p");
-  serverInfo.textContent = "Server Tips: $" + serverTips.toFixed(2);
-  
-  var workersInfo = document.createElement("div");
-  workersInfo.innerHTML = "<h3>Worker Tips:</h3>";
-  
-  workers.forEach(function(worker) {
-    var workerAmount = worker.hoursWorked * workerTips;
-    var workerElement = document.createElement("p");
-    workerElement.textContent = worker.name + ": $" + workerAmount.toFixed(2);
-    workersInfo.appendChild(workerElement);
-  });
-  
-  var container = document.getElementById("tips_container");
-  container.innerHTML = "";
-  container.appendChild(kitchenInfo);
-  container.appendChild(serverInfo);
-  container.appendChild(workersInfo);
-}
+});
